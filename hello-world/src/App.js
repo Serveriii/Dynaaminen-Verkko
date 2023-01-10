6fd5cc88adabdff9d8bac8d830855a7652cc26e0
Serveriii <n2jose00@students.oamk.fi>
2023-01-10 11:53:08 +0200

    React project initial commit

---
+++ hello-world/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <p>Hello world!</p>
+      <img src={logo} class="appLogo" alt="appLogo"></img>
+    </div>
+  );
+}
+
+export default App;
