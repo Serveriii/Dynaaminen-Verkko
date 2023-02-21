2e35a17b5217c63418688014db9521e8bbdb85c2
Serveriii <n2jose00@students.oamk.fi>
2023-02-21 08:20:24 +0200

    Deleted obsolete component

--- api-assignment-jokelainen/src/Videobutton.js
+++
@@ -1,8 +0,0 @@
-import { useEffect } from "react"
-
-export default function Videobutton ({link}) {
-    
-  return (
-      <button className='button'><a href={link}>Tutorial video(Youtube)</a></button>
-  )
-}
